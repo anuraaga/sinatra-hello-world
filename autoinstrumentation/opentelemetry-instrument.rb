58ea929c0b25a3a2faa7540d8472bffbfd723d92
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-11-15 16:05:07 +0900

    Hacky?

--- autoinstrumentation/opentelemetry-instrument.rb
+++ autoinstrumentation/opentelemetry-instrument.rb
@@ -1,5 +1,20 @@
 # Run only if not using bundler, or otherwise in the subprocess running the app, which includes bundler's RUBYOPT
 if !ENV['_'].include?('bundle') || ENV['RUBYOPT'].include?('-rbundler/setup')
+  module Sinatra
+    class Base
+      def self.singleton_method_added(name)
+        if name == :register
+          require 'opentelemetry/sdk'
+          require 'opentelemetry/exporter/otlp'
+          require 'opentelemetry/instrumentation/all'
+          OpenTelemetry::SDK.configure do |c|
+            c.use_all() # enables all instrumentation!
+          end
+        end
+      end
+    end
+  end
+
   require 'bundler'
   require 'yaml'
 
@@ -8,16 +23,4 @@ if !ENV['_'].include?('bundle') || ENV['RUBYOPT'].include?('-rbundler/setup')
   gem_paths = YAML.load(File.read("#{current_dir}/manifest.yml")).map { |path| "#{current_dir}/#{path}" }
 
   $LOAD_PATH.append(*gem_paths)
-
-  # We need to make sure libraries have been required before enabling instrumentation.
-  # But then the app doesn't start for some reason.
-  # Comment out this line and the app will start, but instrumentation will fail.
-  Bundler.require(:default)
-
-  require 'opentelemetry/sdk'
-  require 'opentelemetry/exporter/otlp'
-  require 'opentelemetry/instrumentation/all'
-  OpenTelemetry::SDK.configure do |c|
-    c.use_all() # enables all instrumentation!
-  end
 end
